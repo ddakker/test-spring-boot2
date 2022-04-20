dd9f852eff1936266d80c29c67b743f72959399b
ddakker <lee1474510>
2022-04-20 16:18:35 +0900

    정상적으로 되게..

--- src/main/java/pe/kr/ddakker/spring/boot/mvc/web/MainController.java
+++ src/main/java/pe/kr/ddakker/spring/boot/mvc/web/MainController.java
@@ -1,5 +1,7 @@
 package pe.kr.ddakker.spring.boot.mvc.web;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -10,9 +12,12 @@ import java.io.PrintWriter;
 
 @Controller
 public class MainController {
+    private static Logger logger = LoggerFactory.getLogger(MainController.class);
+
 
     @GetMapping("/")
     public void index(HttpServletResponse res) throws IOException {
+        logger.info("===== main");
         PrintWriter out = res.getWriter();
         out.println("index");
         return;
